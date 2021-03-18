e85cf15392ec925ac10bc1c47940d6ba21f353eb
p-x9 <chi.iosdev@gmail.com>
2021-03-18 18:17:53 +0900

    Initial Commit

---
+++ VideoClip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoClip
+//
+//  Created by p-x9 on 2021/03/18.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
