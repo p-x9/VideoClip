45a8799a990d58f01c7d408cb828aa5e44132057
p-x9 <chi.iosdev@gmail.com>
2021-03-24 01:17:28 +0900

    Add SwiftLint Process and fix warning about it

--- VideoClip/ViewController.swift
+++ VideoClip/ViewController.swift
@@ -10,183 +10,187 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var webView: WKWebView!
-    @IBOutlet var backButton: UIBarButtonItem!
-    @IBOutlet var forwardButton: UIBarButtonItem!
-    
-    
+
     private var searchBar: UISearchBar!
     private var reloadButton: UIBarButtonItem!
     private var progressView = UIProgressView()
-    
+
     private var scrollBeginningPoint: CGPoint = .zero
     private var isViewShowed: Bool!
-    
-    
-    deinit{
-        self.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
-        self.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.isLoading))
-        self.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack))
-        self.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward))
-    }
-    
+    private let defaultSearchEngineURL = URL(string: "https://www.google.com/")
+
+    private var observeEstimatedProgress: NSKeyValueObservation?
+    private var observeIsLoading: NSKeyValueObservation?
+    private var observeCanGoBack: NSKeyValueObservation?
+    private var observeCanGoForward: NSKeyValueObservation?
+
+    @IBOutlet private var webView: WKWebView!
+    @IBOutlet private var backButton: UIBarButtonItem!
+    @IBOutlet private var forwardButton: UIBarButtonItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        webView.load(URLRequest(url: URL(string: "https://www.google.com/")!))
+
+        if let url = defaultSearchEngineURL {
+            webView.load(URLRequest(url: url))
+        }
         webView.allowsBackForwardNavigationGestures = true
-        
+
         webView.navigationDelegate = self
         webView.uiDelegate = self
         webView.scrollView.delegate = self
-        
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack), options: .new, context: nil)
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward), options: .new, context: nil)
-        
+
         setUpSearchBar()
         setUpProgressBar()
+        observeKeysForWebView()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.isViewShowed = true
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.isViewShowed = false
     }
-    
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        switch keyPath {
-        case #keyPath(WKWebView.estimatedProgress):
-            self.progressView.setProgress(Float(self.webView.estimatedProgress), animated: true)
-        case #keyPath(WKWebView.isLoading):
-            if self.webView.isLoading {
-                self.progressView.setProgress(0.1, animated: true)
-            }else{
-                self.progressView.setProgress(0.0, animated: true)
-            }
-        case #keyPath(WKWebView.canGoBack):
-            backButton.isEnabled = !(webView.canGoBack) ? false : true
-        case #keyPath(WKWebView.canGoForward):
-            forwardButton.isEnabled = !(webView.canGoForward) ? false : true
-        default:
-            break
+
+    @IBAction private func handleBackButton(_ sender: Any) {
+        webView.goBack()
+    }
+    @IBAction private func handleForwardButton(_ sender: Any) {
+        webView.goForward()
+    }
+
+    @objc
+    func handleReloadButton(sender: UIButton) {
+        if webView.url == nil ,
+        let url = URL(string: "https://www.google.com/") {
+            webView.load(URLRequest(url: url))
+        } else {
+            self.webView.reload()
         }
     }
-    
-    func setUpSearchBar(){
-        searchBar = UISearchBar(frame:CGRect(x:0, y:0, width:270, height:80))
+
+    func setUpSearchBar() {
+        searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: 270, height: 80))
         searchBar.delegate = self
-        searchBar.layer.position = CGPoint(x: self.view.bounds.width/2, y: 20)
+        searchBar.layer.position = CGPoint(x: self.view.bounds.width / 2, y: 20)
         searchBar.searchBarStyle = UISearchBar.Style.minimal
         searchBar.placeholder = "URLまたは検索ワード"
         searchBar.tintColor = UIColor.blue
-        
+
         searchBar.showsSearchResultsButton = false
         searchBar.showsCancelButton = false
         searchBar.showsBookmarkButton = false
-        
+
         self.navigationItem.titleView = searchBar
-        
+
         // Reload Button
         reloadButton = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(handleReloadButton(sender:)))
         self.navigationItem.rightBarButtonItem = reloadButton
     }
-    
-    func setUpProgressBar(){
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.isLoading), options: .new, context: nil)
-        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
-        
-        progressView = UIProgressView(frame: CGRect(x: 0, y: self.navigationController!.navigationBar.frame.height+10, width: self.view.frame.size.width, height: 10))
+
+    func setUpProgressBar() {
+        progressView = UIProgressView(frame: CGRect(x: 0, y: (self.navigationController?.navigationBar.frame.height ?? 0) + 10, width: self.view.frame.size.width, height: 10))
         progressView.progressViewStyle = .bar
         self.navigationController?.navigationBar.addSubview(progressView)
     }
-    
-    @IBAction func handleBackButton(_ sender: Any) {
-        webView.goBack()
-    }
-    @IBAction func handleForwardButton(_ sender: Any) {
-        webView.goForward()
-    }
-    
-    @objc func handleReloadButton(sender : UIButton){
-        if webView.url == nil{
-            webView.load(URLRequest(url: URL(string: "https://www.google.com/")!))
+
+    func observeKeysForWebView() {
+        self.observeEstimatedProgress = self.webView.observe(\.estimatedProgress, options: [.new]) {_, change in
+            self.progressView.setProgress(Float(change.newValue ?? 0), animated: true)
         }
-        else{
-            self.webView.reload()
+        self.observeIsLoading = self.webView.observe(\.isLoading, options: [.new]) {_, change in
+            if let newValue = change.newValue, newValue {
+                self.progressView.setProgress(0.1, animated: true)
+            } else {
+                self.progressView.setProgress(0.0, animated: true)
+            }
+        }
+        self.observeCanGoBack = self.webView.observe(\.canGoBack, options: [.new]) {webView, _ in
+            self.backButton.isEnabled = !(webView.canGoBack) ? false : true
+        }
+        self.observeCanGoForward = self.webView.observe(\.canGoForward, options: [.new]) {webView, _ in
+            self.forwardButton.isEnabled = !(webView.canGoForward) ? false : true
         }
     }
-    
-    
+
+//    deinit {
+//        self.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress))
+//        self.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.isLoading))
+//        self.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack))
+//        self.webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward))
+//    }
+
 }
 
-//MARK: - WKNavigationDelegate
-extension ViewController: WKNavigationDelegate{
+// MARK: - WKNavigationDelegate
+extension ViewController: WKNavigationDelegate {
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         backButton.isEnabled = !(webView.canGoBack) ? false : true
         forwardButton.isEnabled = !(webView.canGoForward) ? false : true
-        //progressView.setProgress(0, animated: false)
-        
-        if (webView.url?.absoluteString.contains("google."))! && (webView.url?.absoluteString.contains("search"))!{
-            searchBar.text = webView.title?.split(separator: "-").map({String($0)})[0]
+        // progressView.setProgress(0, animated: false)
+
+        guard let url = webView.url else {
+            return
         }
-        else if (webView.url?.absoluteString.contains("https://www.google.com/"))! && [29,23].contains(webView.url?.absoluteString.count){
+
+        if url.absoluteString.contains("google.") && url.absoluteString.contains("search") {
+            searchBar.text = webView.title?.split(separator: "-").map { String($0) }[0]
+        } else if url.absoluteString.contains("https://www.google.com/") && [29, 23].contains(url.absoluteString.count) {
             searchBar.text = ""
-        }
-        else{
+        } else {
             searchBar.text = webView.url?.absoluteString
         }
     }
-    
+
     func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        
+
     }
-    
+
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
         let e = error as NSError
-        if e.code == -1009{
+        if e.code == -1009 {
             print("don't connected to internet")
-        }
-        else{
-            
+        } else {
+
         }
     }
-    
+
 }
 
 // MARK: - WKUIDelegate
-extension ViewController: WKUIDelegate{
+extension ViewController: WKUIDelegate {
     // target=_blank
     func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-        
+
         if navigationAction.targetFrame == nil {
             webView.load(navigationAction.request)
         }
-        
+
         return nil
     }
 }
 
 // MARK: - UISearchBarDelegate
-extension ViewController: UISearchBarDelegate{
+extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        search(urlString: searchBar.text!)
         searchBar.resignFirstResponder()
-    }
-    
-    func search( urlString:String)
-    {
-        var urlString = urlString
-        if(urlString == ""){
-            return;
+
+        if let text = searchBar.text {
+            search(urlString: text)
         }
-        
+    }
+
+    func search( urlString: String) {
+        var urlString = urlString
+        if urlString.isEmpty {
+            return
+        }
+
         var strUrl: String
-        var searchWord:String = ""
+        var searchWord: String = ""
         let chkURL = urlString.components(separatedBy: ".")
         if chkURL.count > 1 {
             // URLの場合
@@ -202,29 +206,30 @@ extension ViewController: UISearchBarDelegate{
             searchWord = words.joined(separator: "+")
             urlString = "https://www.google.co.jp/search?&q=\(searchWord)"
         }
-        
-        let encodedUrlString = urlString.addingPercentEncoding(withAllowedCharacters:NSCharacterSet.urlQueryAllowed)
-        let url = NSURL(string: encodedUrlString!)
-        let request = NSURLRequest(url: url! as URL)
-        self.webView.load(request as URLRequest)
+
+        if let encodedUrlString = urlString.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed),
+        let url = URL(string: encodedUrlString) {
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+        }
     }
 }
 
-extension ViewController: UIScrollViewDelegate{
+extension ViewController: UIScrollViewDelegate {
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         scrollBeginningPoint = scrollView.contentOffset
     }
-    
+
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let currentPoint = scrollView.contentOffset
         let contentSize = scrollView.contentSize
         let frameSize = scrollView.frame
         let maxOffSet = contentSize.height - frameSize.height
-        
+
         if currentPoint.y >= maxOffSet || scrollBeginningPoint.y < currentPoint.y {
             self.navigationController?.setToolbarHidden(true, animated: true)
             self.navigationController?.setNavigationBarHidden(true, animated: true)
-        } else{
+        } else {
             self.navigationController?.setToolbarHidden(false, animated: true)
             self.navigationController?.setNavigationBarHidden(false, animated: true)
         }
